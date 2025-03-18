36afa4e1e997746494b797ed90dc3a6a147541c9
Satyam Kumar <Satyam.Kumar@winjit.com>
2025-03-18 14:07:58 +0530

    added function in start quiz button

--- lib/front_page.dart
+++ lib/front_page.dart
@@ -1,37 +1,42 @@
 import 'package:flutter/material.dart';
 
 class FrontPage extends StatelessWidget {
-  const FrontPage({super.key});
+  const FrontPage(this.switchScreen, {super.key});
+  final void Function() switchScreen;
   @override
   Widget build(context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 250,
-          color: const Color.fromARGB(149, 255, 255, 255),
-        ),
-        const SizedBox(height: 40),
-        Text(
-          'Learn Flutter the fun way!',
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.yellow,
-            decoration: TextDecoration.none,
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 250,
+            color: const Color.fromARGB(149, 255, 255, 255),
           ),
-        ),
-        const SizedBox(height: 20),
-        OutlinedButton.icon(
-          onPressed: () {},
-          style: ButtonStyle(
-            shape: WidgetStateProperty.all(RoundedRectangleBorder()),
+          const SizedBox(height: 40),
+          Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.yellow,
+              decoration: TextDecoration.none,
+            ),
           ),
-          icon: Icon(Icons.arrow_right_alt, color: Colors.white),
-          label: Text('Start Quiz', style: TextStyle(color: Colors.white)),
-        ),
-      ],
+          const SizedBox(height: 20),
+          OutlinedButton.icon(
+            onPressed: () {
+              switchScreen();
+            },
+            style: ButtonStyle(
+              shape: WidgetStateProperty.all(RoundedRectangleBorder()),
+            ),
+            icon: Icon(Icons.arrow_right_alt, color: Colors.white),
+            label: Text('Start Quiz', style: TextStyle(color: Colors.white)),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/front_page.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 51, 2, 59),
-                const Color.fromARGB(255, 126, 125, 125),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(child: FrontPage()),
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-    ),
-  );
+  runApp(Quiz());
 }
