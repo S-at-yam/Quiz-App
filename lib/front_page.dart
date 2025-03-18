bb6f78b0074cf2dcbd4a85ab52605532acd650ea
Satyam Kumar <Satyam.Kumar@winjit.com>
2025-03-18 12:11:45 +0530

    Finished front page

--- lib/front_page.dart
+++ lib/front_page.dart
@@ -4,32 +4,34 @@ class FrontPage extends StatelessWidget {
   const FrontPage({super.key});
   @override
   Widget build(context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.deepPurple),
-
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/images/quiz-logo.png', width: 250),
-          const SizedBox(height: 20),
-          Text(
-            'Learn Flutter the fun way!',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
-              decoration: TextDecoration.none,
-            ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 250,
+          color: const Color.fromARGB(149, 255, 255, 255),
+        ),
+        const SizedBox(height: 40),
+        Text(
+          'Learn Flutter the fun way!',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.yellow,
+            decoration: TextDecoration.none,
           ),
-          const SizedBox(height: 20),
-          OutlinedButton(
-            onPressed: () {},
-            style: ButtonStyle(
-              shape: WidgetStateProperty.all(RoundedRectangleBorder()),
-            ),
-            child: Text('Start Quiz', style: TextStyle(color: Colors.white)),
+        ),
+        const SizedBox(height: 20),
+        OutlinedButton.icon(
+          onPressed: () {},
+          style: ButtonStyle(
+            shape: WidgetStateProperty.all(RoundedRectangleBorder()),
           ),
-        ],
-      ),
+          icon: Icon(Icons.arrow_right_alt, color: Colors.white),
+          label: Text('Start Quiz', style: TextStyle(color: Colors.white)),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,21 @@ import 'package:quiz_app/front_page.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(body: FrontPage()),
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 51, 2, 59),
+                const Color.fromARGB(255, 126, 125, 125),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(child: FrontPage()),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
     ),
   );
