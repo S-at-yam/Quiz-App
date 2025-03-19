8a79792b430b6c27a321ac4c3d8ea251a5f08276
Satyam Kumar <Satyam.Kumar@winjit.com>
2025-03-19 18:02:23 +0530

    made results scrollable

--- lib/summary_page.dart
+++ lib/summary_page.dart
@@ -5,26 +5,32 @@ class SummaryPage extends StatelessWidget {
   final List<Map<String, Object>> summaryData;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children:
-          summaryData.map((data) {
-            return Row(
-              children: [
-                Text(((data['questionIndex'] as int) + 1).toString()),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text(data['question'] as String),
-                      const SizedBox(height: 5),
-                      Text(data['correctAnswer'] as String),
-                      const SizedBox(height: 5),
-                      Text(data['selectedAnswer'] as String),
-                    ],
-                  ),
-                ),
-              ],
-            );
-          }).toList(),
+    return SizedBox(
+      height: 300,
+      width: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children:
+              summaryData.map((data) {
+                return Row(
+                  children: [
+                    Text(((data['questionIndex'] as int) + 1).toString()),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text(data['question'] as String),
+                          const SizedBox(height: 5),
+                          Text(data['correctAnswer'] as String),
+                          const SizedBox(height: 5),
+                          Text(data['selectedAnswer'] as String),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }).toList(),
+        ),
+      ),
     );
   }
 }
