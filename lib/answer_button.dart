a4ffd5078b2e0b556f9295ce20c7c45d81bbd73e
Satyam Kumar <Satyam.Kumar@winjit.com>
2025-03-19 14:22:52 +0530

    completed Answer Button and its styling

---
+++ lib/answer_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.text, this.onTap, {super.key});
+
+  final String text;
+  final void Function() onTap;
+  @override
+  Widget build(context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.65,
+      child: ElevatedButton(
+        onPressed: onTap,
+
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.elliptical(10, 5)),
+          ),
+          foregroundColor: Colors.white,
+          backgroundColor: const Color.fromARGB(188, 207, 29, 88),
+          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        ),
+
+        child: Text(text, style: TextStyle(fontSize: 17)),
+      ),
+    );
+  }
+}
--- lib/question_page.dart
+++ lib/question_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/models/quiz_questions.dart';
 
 class QuestionPage extends StatefulWidget {
   const QuestionPage({super.key});
@@ -11,12 +14,22 @@ class QuestionPage extends StatefulWidget {
 class _QuestionPageState extends State<QuestionPage> {
   @override
   Widget build(context) {
+    final QuizQuestion curQues = questions[0];
     return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Question'),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Text(
+              curQues.text,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
           const SizedBox(height: 30),
-          AnswerButton(),
+          ...curQues.answers.map((item) {
+            return AnswerButton(item, () {});
+          }),
         ],
       ),
     );
